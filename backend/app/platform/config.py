552f616183b4a8c7b662ebce4210d83aea77d7b4
Binbin Liu <bliu@c1r3s12.42lisboa.com>
2026-07-17 16:06:48 +0100

    align the env.example file and setting

--- backend/app/platform/config.py
+++ backend/app/platform/config.py
@@ -7,7 +7,6 @@ from pydantic import (
     BeforeValidator,
     EmailStr,
     HttpUrl,
-    PostgresDsn,
     computed_field,
     model_validator,
 )
@@ -24,24 +23,17 @@ def parse_cors(v: Any) -> list[str] | str:
 
 
 class Settings(BaseSettings):
-    # model_config = SettingsConfigDict(
-    #     # Use top level .env file (one level above ./backend/)
-    #     env_file="../.env",
-    #     env_ignore_empty=True,
-    #     extra="ignore",
-    # )
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
-    # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    SECRET_KEY: str = secrets.token_urlsafe(32)
     FRONTEND_HOST: str = "http://localhost:5173"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    DATABASE_URL: str = "need to set"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
     ] = []
 
-    @computed_field  # type: ignore[prop-decorator]
+    @computed_field
     @property
     def all_cors_origins(self) -> list[str]:
         return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
@@ -49,40 +41,9 @@ class Settings(BaseSettings):
         ]
 
     PROJECT_NAME: str = "UNO"
-    SENTRY_DSN: HttpUrl | None = None
-    # POSTGRES_SERVER: str
-    # POSTGRES_PORT: int = 5432
-    # POSTGRES_USER: str
-    # POSTGRES_PASSWORD: str = ""
-    # POSTGRES_DB: str = ""
-
-    # @computed_field  # type: ignore[prop-decorator]
-    # @property
-    # def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-    #     return PostgresDsn.build(
-    #         scheme="postgresql+psycopg",
-    #         username=self.POSTGRES_USER,
-    #         password=self.POSTGRES_PASSWORD,
-    #         host=self.POSTGRES_SERVER,
-    #         port=self.POSTGRES_PORT,
-    #         path=self.POSTGRES_DB,
-    #     )
-
-    # @model_validator(mode="after")
-    # def _set_default_emails_from(self) -> Self:
-    #     if not self.EMAILS_FROM_NAME:
-    #         self.EMAILS_FROM_NAME = self.PROJECT_NAME
-    #     return self
-
     EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
 
-    # @computed_field  # type: ignore[prop-decorator]
-    # @property
-    # def emails_enabled(self) -> bool:
-    #     return bool(self.SMTP_HOST and self.EMAILS_FROM_EMAIL)
-
-    EMAIL_TEST_USER: EmailStr = "test@example.com"
-    FIRST_SUPERUSER: EmailStr ="superuser@example.com"
+    FIRST_SUPERUSER: EmailStr ="not set"
     FIRST_SUPERUSER_PASSWORD: str = "changethis"
 
     def _check_default_secret(self, var_name: str, value: str | None) -> None:
@@ -91,21 +52,15 @@ class Settings(BaseSettings):
                 f'The value of {var_name} is "changethis", '
                 "for security, please change it, at least for deployments."
             )
-            if self.ENVIRONMENT == "local":
-                warnings.warn(message, stacklevel=1)
-            else:
-                raise ValueError(message)
+            raise ValueError(message)
 
     @model_validator(mode="after")
     def _enforce_non_default_secrets(self) -> Self:
         self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
-        # self._check_default_secret("POSTGRES_PASSWORD", self.POSTGRES_PASSWORD)
         self._check_default_secret(
             "FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD
         )
 
         return self
 
-
-settings = Settings()  # type: ignore
-print(settings.dict())
+settings = Settings()
