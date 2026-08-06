1549687a4254a70f658b902a56f5db431bbb752b
Your Name <your.email@example.com>
2026-08-06 17:55:36 +0100

    add sign for user

--- backend/app/models/all.py
+++ backend/app/models/all.py
@@ -17,6 +17,7 @@ class UserBase(SQLModel):
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
     avatar: str | None = Field(default=None, max_length=255)
+    use2fa: bool = False
 
 
 # Properties to receive via API on creation
