16fd7e5a22f3e82c9963a40f4bcb56f684201753
H.L <131590624+HL-PT@users.noreply.github.com>
2026-08-16 17:47:59 +0100

    bug fix

--- backend/app/platform/routes/friend.py
+++ backend/app/platform/routes/friend.py
@@ -1,14 +1,9 @@
-from datetime import datetime, timezone
 import logging
 
-from app.platform.service.userStatisticService import calculate_level_data
 from app.platform.service import friendservice
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select, and_, or_
+from fastapi import APIRouter
 from app.platform.deps import CurrentUser, SessionDep
-from app.models.all import Friends, Suggestions, Requests, Friend, FriendshipStatus, Friendship, User, UserStatistic
-from uuid import UUID, uuid4
-from app.presence_manager import presence_manager
+from app.models.all import Friends, Suggestions, Requests, FriendshipStatus, Friendship
 
 
 router = APIRouter(prefix="/friends", tags=["friends"])
@@ -20,7 +15,9 @@ async def get_suggested_friends(session: SessionDep, current_user: CurrentUser):
     Get a list of suggested friends for the current user.
     A suggested friend is a user who is not the current user, is not a superuser, is active, and is not already a friend or has a pending friend request with the current user
     '''
+
     friends = friendservice.get_suggested_friends(session=session, current_user=current_user)
+
     return {"suggestions": friends, "count": len(friends)}
 
 
@@ -31,6 +28,7 @@ async def get_all_friends(session: SessionDep, current_user: CurrentUser, skip: 
     '''
     
     friends = friendservice.get_all_friends(session=session, current_user=current_user, skip=skip, limit=limit)
+
     return {"friends": friends, "count": len(friends)}
 
 @router.get("/pending", response_model=Requests)
@@ -38,7 +36,9 @@ async def get_pending_friends(session: SessionDep, current_user: CurrentUser, sk
     '''
     Get a list of all pending friend requests for the current user.
     '''
+
     friends = friendservice.get_pending_friends(session=session, current_user=current_user, skip=skip, limit=limit)
+
     return {"requests": friends, "count": len(friends)}
 
 @router.post("/add/{friend_id}", response_model=Friendship)
@@ -46,6 +46,7 @@ async def add_friend(friend_id: str, session: SessionDep, current_user: CurrentU
     '''
     Send a friend request to another user.
     '''
+
     friendship = friendservice.add_friend(friend_id=friend_id, session=session, current_user=current_user)
 
     return friendship
@@ -55,6 +56,7 @@ async def accept_friend(friend_id: str, status: FriendshipStatus, session: Sessi
     '''
     Accept, block, or reject a friend request from another user.
     '''
+    
     friendship = friendservice.accept_friend(friend_id=friend_id, status=status, session=session, current_user=current_user)
     
     return friendship
--- backend/app/platform/service/friendservice.py
+++ backend/app/platform/service/friendservice.py
@@ -1,4 +1,5 @@
 from sqlmodel import Session, select, and_, or_
+from datetime import datetime, timezone
 
 from app.models.all import APIError, APIErrorCode, Friend, Friendship, FriendshipStatus, User, UserStatistic
 from app.platform.deps import CurrentUser, SessionDep
@@ -11,7 +12,7 @@ def get_suggested_friends(session: Session, current_user: CurrentUser)-> list[Fr
     Get a list of suggested friends for the current user.
     A suggested friend is a user who is not the current user, is not a superuser, is active, and is not already a friend or has a pending friend request with the current user
     '''
-    statement = select(User,UserStatistic).join(UserStatistic, User.id == UserStatistic.user_id).where(User.id != current_user.id,User.is_superuser == False,User.is_active == True,
+    statement = select(User,UserStatistic).join(UserStatistic, User.id == UserStatistic.user_id, isouter=True).where(User.id != current_user.id,User.is_superuser == False,User.is_active == True,
                                     User.id.not_in(
                                         select(Friendship.addressee_id).where(Friendship.requester_id == current_user.id)
                                     ),
@@ -20,7 +21,7 @@ def get_suggested_friends(session: Session, current_user: CurrentUser)-> list[Fr
                                     )).limit(42)
     users_result = session.exec(statement).all()
     user_requested_result = session.exec(
-        select(User, UserStatistic).join(Friendship, and_(User.id == Friendship.addressee_id, Friendship.status == FriendshipStatus.PENDING,Friendship.requester_id == current_user.id)).join(UserStatistic, User.id == UserStatistic.user_id).where(User.is_superuser == False,User.is_active == True)
+        select(User, UserStatistic).join(Friendship, and_(User.id == Friendship.addressee_id, Friendship.status == FriendshipStatus.PENDING,Friendship.requester_id == current_user.id)).join(UserStatistic, User.id == UserStatistic.user_id, isouter=True).where(User.is_superuser == False,User.is_active == True)
     ).all()
 
     friends = []
@@ -37,20 +38,20 @@ def get_all_friends(session: Session, current_user: CurrentUser, skip: int = 0, 
     Get a list of all friends for the current user.
     A friend is a user who has an accepted friendship with the current user.
     '''
-    statement = select(User, Friendship).join(Friendship, or_(
+    statement = select(User, Friendship, UserStatistic).join(Friendship, or_(
         (Friendship.requester_id == current_user.id) & (Friendship.addressee_id == User.id),
         (Friendship.addressee_id == current_user.id) & (Friendship.requester_id == User.id)
-    )).where(Friendship.status == FriendshipStatus.ACCEPTED).offset(skip).limit(limit)
+    )).join(UserStatistic, User.id == UserStatistic.user_id, isouter=True).where(Friendship.status == FriendshipStatus.ACCEPTED).offset(skip).limit(limit)
     result = session.exec(statement).all()
     friends = []
     if result:
-        for user, friendship in result:
+        for user, friendship, user_statistic in result:
             blocked = False
             if friendship.requester_id == current_user.id:
                 blocked = bool(friendship.blocked_by_req)
             elif friendship.addressee_id == current_user.id:
                 blocked = bool(friendship.blocked_by_add)
-            friends.append(Friend(id=user.id, name=user.full_name, handle=user.full_name, avatar=user.avatar, status=(FriendshipStatus.BLOCKED if blocked else FriendshipStatus.ACCEPTED), online=presence_manager.is_online(str(user.id))))
+            friends.append(Friend(id=user.id, name=user.full_name, handle=user.full_name, avatar=user.avatar, level=calculate_level_data(user_statistic.total_score).current_level, title=calculate_level_data(user_statistic.total_score).title, status=(FriendshipStatus.BLOCKED if blocked else FriendshipStatus.ACCEPTED), online=presence_manager.is_online(str(user.id))))
 
     return friends
 
@@ -60,12 +61,13 @@ def get_pending_friends(session: Session, current_user: CurrentUser, skip: int =
     Get a list of all pending friend requests for the current user.
     A pending friend request is a user who has sent a friend request to the current user, and the current user has not yet accepted or rejected the request.
     '''
-    statement = select(User).join(Friendship, and_(Friendship.addressee_id == current_user.id,Friendship.requester_id == User.id)
-        ).where(Friendship.status == FriendshipStatus.PENDING, User.is_superuser == False,User.is_active == True).offset(skip).limit(limit)
-    users = session.exec(statement).all()
+    statement = select(User,UserStatistic).join(Friendship, and_(Friendship.addressee_id == current_user.id,Friendship.requester_id == User.id)
+        ).join(UserStatistic, User.id == UserStatistic.user_id, isouter=True).where(Friendship.status == FriendshipStatus.PENDING, User.is_superuser == False,User.is_active == True).offset(skip).limit(limit)
+    result = session.exec(statement).all()
     friends = []
-    if users:
-        friends = [Friend(id=user.id, name=user.full_name, handle=user.full_name, avatar=user.avatar, status=FriendshipStatus.PENDING) for user in users]
+    if result:
+        for user, user_statistic in result:
+            friends.append(Friend(id=user.id, name=user.full_name, handle=user.full_name, avatar=user.avatar, level=calculate_level_data(user_statistic.total_score).current_level, title=calculate_level_data(user_statistic.total_score).title, status=FriendshipStatus.PENDING))
 
     return friends
 
